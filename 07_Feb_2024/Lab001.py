205a3d3e23525e6f7a3abc9efe0c10e995272962
ShambhaviKu <158810332+ShambhaviKu@users.noreply.github.com>
2024-02-07 22:43:52 +0530

    2nd commit

---
+++ 07_Feb_2024/Lab001.py
@@ -0,0 +1,15 @@
+list = [1,2,3,4,5,6,6,4]
+print(list)
+
+set = {1,2,3,4,5,'hi',6,1}
+print(set)
+
+a = []
+a.insert(0,1)
+a.insert(1,10)
+a.insert(2,23)
+a.insert(2,78)
+a.insert(3,12)
+a.append(985)
+print(a)
+

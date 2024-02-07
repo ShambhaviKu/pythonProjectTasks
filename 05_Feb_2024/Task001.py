aacf7c4282216f1e07c82f44eaf3d0ae46ec68bc
ShambhaviKu <158810332+ShambhaviKu@users.noreply.github.com>
2024-02-07 14:53:13 +0530

    Add Task

---
+++ 05_Feb_2024/Task001.py
@@ -0,0 +1,29 @@
+print("Table of 2")
+print('----------')
+print(2 * 1)
+print(2 * 2)
+print(2 * 3)
+print(2 * 4)
+print(2 * 5)
+print(2 * 6)
+print(2 * 7)
+print(2 * 8)
+print(2 * 9)
+print(2 * 10)
+
+print('----------')
+
+for i in range(1, 11):
+    print(i * 2)
+
+number = 90
+print(f"table of {number}:", number * 1)
+print(f"table of {number}:", number * 2)
+print(f"table of {number}:" ,number * 3)
+print(f"table of {number}:", number * 4)
+print(f"table of {number}:", number * 5)
+print(f"table of {number}:", number * 6)
+print(f"table of {number}:", number * 7)
+print(f"table of {number}:", number * 8)
+print(f"table of {number}:", number * 9)
+print(f"table of {number}:", number * 10)

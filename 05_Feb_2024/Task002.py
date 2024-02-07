9a1f1b201bb6054caa323d1cba070a5599b65a84
ShambhaviKu <158810332+ShambhaviKu@users.noreply.github.com>
2024-02-07 15:20:10 +0530

    Add Task

---
+++ 05_Feb_2024/Task002.py
@@ -0,0 +1,18 @@
+
+Name = input("Enter your Name: ")
+print(Name)
+
+Age = input("Enter your Age: ")
+print(Age)
+
+ID = input("Enter your ID: ")
+print(ID)
+
+Contact = input("Enter your phone number: ")
+print(Contact)
+
+first_name = "Shambhavi"
+last_name = "Kulkarni"
+age = 28
+isMarried = True
+print(f'My name is {first_name} {last_name}\n My age is {age}\n  I am married {isMarried}')

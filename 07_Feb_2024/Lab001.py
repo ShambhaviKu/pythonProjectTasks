ddcd51e8f4a996e41ac5709dad0df71c4c4e239c
ShambhaviKu <158810332+ShambhaviKu@users.noreply.github.com>
2024-02-07 23:38:52 +0530

    3rd commit: list and set

--- 07_Feb_2024/Lab001.py
+++ 07_Feb_2024/Lab001.py
@@ -1,8 +1,13 @@
-list = [1,2,3,4,5,6,6,4]
-print(list)
-
+#set operator
 set = {1,2,3,4,5,'hi',6,1}
+set2 = {1,2,3,4}
 print(set)
+
+#list operator
+list = [1,2,3,4,5,6,6,4]
+list[0] = 10
+list.reverse()
+print(list)
 
 a = []
 a.insert(0,1)
@@ -11,5 +16,15 @@ a.insert(2,23)
 a.insert(2,78)
 a.insert(3,12)
 a.append(985)
+a.remove(1)
+a.reverse()
+a.extend([20,21,23])
+a.pop(1)
+a.pop()
+print(a[2:5])
 print(a)
 
+
+#Bitwise operator
+b = 8^7
+print(b)

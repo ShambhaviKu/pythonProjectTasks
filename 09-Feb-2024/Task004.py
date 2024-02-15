b9f5cd12bfeea1ec50f15393ab4183f4d332435e
ShambhaviKu <158810332+ShambhaviKu@users.noreply.github.com>
2024-02-15 16:45:12 +0530

    Fibonacci Series

--- 09-Feb-2024/Task004.py
+++ 09-Feb-2024/Task004.py
@@ -1,4 +1,4 @@
-#Fibonaci series
+# Fibonacci series
 
 # 0,0+1, 0+1+1,
 
@@ -6,9 +6,7 @@
 
 # 0, 1, 2, 3, 5, 8, 13
 
-
-i = 0
-while i < 13:  #0
-    i = i + 1   # 0+1=1  3 = 2 + 1 num = (num - 1) + (num - 2)
-    print(i)
-
+a, b = 0, 1
+while a <= 10:
+    print(a, end="|")
+    a, b = b, a + b
